d67e0cba9a18499a14cb0e9af05166402fb51109
Lucas Simone <lucassimone99@gmail.com>
2024-02-17 22:33:34 -0500

    Cleaned up leftovers from refernces and project page

--- main_site/urls.py
+++ main_site/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('resume/', views.resume, name='site-resume'),
     # path('about/', views.about, name='site-about'),
     path('contact/', views.contact, name='site-contact'),
-    path('references/', views.references, name='site-references'),
+    # path('references/', views.references, name='site-references'),
     path('geolocate/', views.geolocate, name='site-geolocate'),
     path('gallery/', views.gallery, name='site-gallery'),
 ]
