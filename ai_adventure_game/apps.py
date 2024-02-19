564767dc24c20d44f68561a5bc50b5144e19893f
Lucas Simone <lucassimone99@gmail.com>
2024-02-18 19:29:31 -0500

    Added requirements.txt

--- ai_adventure_game/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiAdventureGameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_adventure_game'
--- ai_adventure_game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='adventure_game-home'),
-    path('game', views.game, name='adventure_game-game'),
-]
