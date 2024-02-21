bdae297784abe6e8f513c864948fc8a1b16fd4fc
Lucas Simone <lucassimone99@gmail.com>
2024-02-20 22:40:37 -0500

    Small css changes and added default debug setting

--- master_template/settings.py
+++ master_template/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG',default=False)
 
 # Allowed hosts settings
 if os.getenv('USE_PROD_HOSTS', '') == 'True':  # Use production allowed hosts
