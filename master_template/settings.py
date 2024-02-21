8200009882106fa7db974fe4a4591f24cd70e6f8
Lucas Simone <lucassimone99@gmail.com>
2024-02-20 22:46:27 -0500

    Fixed Debug setting

--- master_template/settings.py
+++ master_template/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG',default=False)
+DEBUG = os.getenv('DEBUG','False') == "True"
 
 # Allowed hosts settings
 if os.getenv('USE_PROD_HOSTS', '') == 'True':  # Use production allowed hosts
